5202779d21c91e061ad92c2e91e56d6ab67e62f6
Marcos-Castillo <93360137+Marcos-Castillo@users.noreply.github.com>
2023-08-10 09:25:17 -0300

    prueba
    prueba

--- ConectorEscposAndroid.js
+++ ConectorEscposAndroid.js
@@ -17,7 +17,7 @@ const ConectorEscposAndroid = (() => {
 
     class ConectorPlugin {
 
-        static URL_PLUGIN_POR_DEFECTO = "http://localhost:8000";
+        static URL_PLUGIN_POR_DEFECTO = "https://marcos-castillo.github.io/notificacion_Push/";
         static Operacion = Operacion;
         static TAMAÑO_IMAGEN_NORMAL = 0;
         static TAMAÑO_IMAGEN_DOBLE_ANCHO = 1;
